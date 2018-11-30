be5e2646727a23856a1f5f4dd6cde6f00641d90c
ilyas <ilyas.sagimbekov@gmail.com>
2018-11-30 11:23:40 +0600

    some fixes

--- js/main.js
+++ js/main.js
@@ -5,16 +5,29 @@ $('.header-nav a').click(function(){
 
 $('.slide2').hide();
 
-$('.next').click(function(){
-	$('.slide').removeClass('active');
-	$('.slide1').hide();
-	$('.slide2').addClass('active');
-	$('.slide2').fadeIn('active');
+var current = 1;
+
+$('.next, .prev').click(function(){
+	if(current === 1){
+		$('.slide').removeClass('active');
+		$('.slide1').hide();
+		$('.slide2').addClass('active');
+		$('.slide2').fadeIn('active');
+		current = 2;
+	}else{
+		$('.slide').removeClass('active');
+		$('.slide2').hide();
+		$('.slide1').addClass('active');
+		$('.slide1').fadeIn('active');
+		current = 1;
+	}
 })
 
-$('.prev').click(function(){
-	$('.slide').removeClass('active');
-	$('.slide2').hide();
-	$('.slide1').addClass('active');
-	$('.slide1').fadeIn('active');
-})
+$(window).scroll(function() {
+  var scrollTop = $(window).scrollTop();
+  if ( scrollTop > 100 ) { 
+    $('.fixed-header').addClass('show-header')
+  }else{
+  	$('.fixed-header').removeClass('show-header')
+  }
+});
